366ee37f6fa37936b4c1a985d34f57f5215303b0
globglogabgaIlab <github.alpixx@gmail.com>
2021-07-08 12:51:04 +0300

    Module 2 Task 2

--- src/Models/Task.php
+++ src/Models/Task.php
@@ -1,14 +1,8 @@
 <?php
-
 namespace TaskForce\Models;
 
 class Task
 {
-    private string $currStatus;
-    private int $userID;
-    private int $workerID;
-    private string $status;
-    private string $action;
     // "Marker" in which state the job is in
     // ONE Task - only ONE Status
     const STATUS_NEW = 'status_new';
@@ -24,69 +18,71 @@ class Task
     const ACTION_FAILED = 'action_failed';
 
     const STATUS_ACTION_MAP = [
-            self::STATUS_NEW => [self::ACTION_START, self::ACTION_CANCEL],
-            self::STATUS_CANCELLED => [self::ACTION_CANCEL],
-            self::STATUS_IN_PROGRESS => [self::ACTION_START, self::ACTION_FAILED],
-            self::STATUS_DONE => [self::ACTION_DONE],
-            self::STATUS_FAILED => [self::ACTION_FAILED]
-        ];
+        self::STATUS_NEW => [self::ACTION_START, self::ACTION_CANCEL],
+        self::STATUS_IN_PROGRESS => [self::ACTION_START, self::ACTION_FAILED]
+    ];
     const ACTION_STATUS_MAP = [
-            self::ACTION_START => [self::STATUS_IN_PROGRESS],
-            self::ACTION_DONE => [self::STATUS_DONE],
-            self::ACTION_CANCEL => [self::STATUS_CANCELLED],
-            self::ACTION_FAILED => [self::STATUS_FAILED]
-        ];
+        self::ACTION_START => [self::STATUS_IN_PROGRESS],
+        self::ACTION_DONE => [self::STATUS_DONE],
+        self::ACTION_CANCEL => [self::STATUS_CANCELLED],
+        self::ACTION_FAILED => [self::STATUS_FAILED]
+    ];
+
     /**
-    * Class constructor
-    * 
-    * Invoked when creating a new object of class
-    * Collects job status, customer id and performer id
-    *
-    * @param string $currStatus, int userID, int workerID
-    * @return void
-    */
-    public function __construct(string $currStatus, int $userID, int $workerID) 
+     * Class constructor
+     *
+     * Invoked when creating a new object of class
+     * Collects job status, customer id and performer id
+     *
+     * @param string $currStatus , int userID, int workerID
+     * @return void
+     */
+    public function __construct(string $current_status, int $user_id, int $executor_id)
     {
-        $this->currStatus = $currStatus;
-        $this->userID = $userID;
-        $this->workerID = $workerID;
+        $this->current_status = $current_status;
+        $this->user_id = $user_id;
+        $this->executor_id = $executor_id;
     }
 
     /**
-    * Get Available actions for current status
-    * 
-    * Takes $status and returns a list of 0 to 2 elements
-    * (0 elements if you assign a status that is not in the STATUS_ACTION_MAP dictionary)
-    * Array contains available actions
-    * 
-    * @param string $status
-    * @return array
-    */
-    public function getAllowActionsForStatus(string $status): array 
+     * Get Available actions for current status
+     *
+     * Takes $status and returns a list of 0 to 2 elements
+     * (0 elements if you assign a status that is not available for further actions)
+     * Array contains action classes
+     *
+     * @param string $status
+     * @return array
+     */
+    public function getAllowedActionsForStatus(string $status): array
     {
-        if (array_key_exists($status, self::STATUS_ACTION_MAP)) {
-            return self::STATUS_ACTION_MAP[$status];
+        if ($status == self::STATUS_NEW) {
+            return [new Actions\action_start(), new Actions\action_cancel()];
+        }
+        if ($status == self::STATUS_IN_PROGRESS) {
+            return [new Actions\action_done(), new Actions\action_failed()];
         }
         return [];
     }
 
     /**
-    * Provides information on which status to assign
-    * depending on the Action
-    * 
-    * Takes $status and returns a list of 0 to 2 elements
-    * (0 elements if you assign a status that is not in the STATUS_ACTION_MAP dictionary)
-    * Array contains available actions
-    * 
-    * @param string $action
-    * @return array
-    */
-    public function getStatusForAction(string $action): array 
+     * Provides information on which status to assign
+     * depending on the Action
+     *
+     * Takes $status and returns a list of 0 to 2 elements
+     * (0 elements if you assign a status that is not in the STATUS_ACTION_MAP dictionary)
+     * Array contains available actions
+     *
+     * @param string $action
+     * @return array
+     */
+    public function getStatusForAction(string $action): array
     {
         if (array_key_exists($action, self::ACTION_STATUS_MAP)) {
             return self::ACTION_STATUS_MAP[$action];
         }
         return [];
     }
+
 
 }
--- tests/TaskClassTest.php
+++ tests/TaskClassTest.php
@@ -1,9 +1,14 @@
 <?php
-require_once "../vendor/autoload.php";
 
+use TaskForce\Models\Actions\action_cancel;
+use TaskForce\Models\Actions\action_done;
+use TaskForce\Models\Actions\action_failed;
+use TaskForce\Models\Actions\action_start;
+
+require_once "../vendor/autoload.php";
 $task = new TaskForce\Models\Task("status", 0, 0);
 
-function assert_handler($file, $line, $code) 
+function assert_handler($file, $line, $code)
 {
 	print_r("False assert
 File: $file
@@ -11,17 +16,16 @@ Line: $line
 Code: $code
 ");
 }
+
 assert_options(ASSERT_CALLBACK, 'assert_handler');
-assert($task->getAllowActionsForStatus($task::STATUS_NEW) == [$task::ACTION_START, $task::ACTION_CANCEL]);
-assert($task->getAllowActionsForStatus($task::STATUS_CANCELLED) == [$task::ACTION_CANCEL]);
-assert($task->getAllowActionsForStatus($task::STATUS_IN_PROGRESS) == [$task::ACTION_START, $task::ACTION_FAILED]);
-assert($task->getAllowActionsForStatus($task::STATUS_DONE) == [$task::ACTION_DONE]);
-assert($task->getAllowActionsForStatus($task::STATUS_NEW) == [$task::ACTION_START, $task::ACTION_CANCEL]);
+assert($task->getAllowedActionsForStatus($task::STATUS_NEW)[0]->get_inner_name() == "action_start");
+assert($task->getAllowedActionsForStatus($task::STATUS_NEW)[1]->get_inner_name() == "action_cancel");
+assert($task->getAllowedActionsForStatus($task::STATUS_IN_PROGRESS)[0]->get_inner_name() == "action_done");
+assert($task->getAllowedActionsForStatus($task::STATUS_IN_PROGRESS)[1]->get_inner_name() == "action_failed");
 
-assert($task->getStatusForAction($task::ACTION_START) == [$task::STATUS_IN_PROGRESS]);
-assert($task->getStatusForAction($task::ACTION_DONE) == [$task::STATUS_DONE]);
-assert($task->getStatusForAction($task::ACTION_CANCEL) == [$task::STATUS_CANCELLED]);
-assert($task->getStatusForAction($task::ACTION_FAILED) == [$task::STATUS_FAILED]);
+assert($task->getAllowedActionsForStatus($task::STATUS_NEW)[0]->id_checker(1, 1, 2, "status_new") == true);
+assert($task->getAllowedActionsForStatus($task::STATUS_NEW)[1]->id_checker(1, 1, 2, "status_in_progress") == false);
+assert($task->getAllowedActionsForStatus($task::STATUS_IN_PROGRESS)[0]->id_checker(1, 2, 1, "status_in_progress") == true);
+assert($task->getAllowedActionsForStatus($task::STATUS_IN_PROGRESS)[1]->id_checker(1, 2, 1, "status_new") == false);
 
-print_r("All working fine")
-?>
+print_r("All working fine");
